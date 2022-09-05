23c97d2ced5175e7fbf15c05939ee8b90ea86982
Olyazarichna <olya.zarichna@gmail.com>
2022-09-05 23:54:24 +0300

    date

--- src/components/FormatedDate/FormatedDate.jsx
+++ src/components/FormatedDate/FormatedDate.jsx
@@ -1,24 +1,27 @@
-export const FormatedDate=(props)=>{
-    console.log('props',props);
-//     let days = [
-//         "Sunday",
-//         "Monday",
-//         "Tuesday",
-//         "Wednesday",
-//         "Thursday",
-//         "Friday",
-//         "Saturday"
-//       ];
-// let day = days[props.date.getDay()];
-// let hours = props.date.getHours();
-//     let minutes =props.date.getMinutes();
-//     if(hours <10 || minutes <10) {
-//         hours= `0${hours}`;
-//         minutes =`0${minutes}`;
+export const FormatedDate = (props) => {
+  console.log("props", props);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+//   let day = days[props.date.getDay()];
+//   let hours = props.date.getHours();
+//   let minutes = props.date.getMinutes();
+//     if (hours < 10 || minutes < 10) {
+//       hours = `0${hours}`;
+//       minutes = `0${minutes}`;
 //     }
-    return(
-        <>
-         {/* <div>{day} {hours}:{minutes}</div> */}
-        </>
-    )
-}
+  return (
+    <>
+      <div>
+        {/* {day}
+        {hours}:{minutes} */}
+      </div>
+    </>
+  );
+};
